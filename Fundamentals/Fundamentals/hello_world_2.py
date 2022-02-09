14357fe75ec9779d74baf7d899ce73ea52c6508e
katoltmans <katoltmans@msn.com>
2022-02-08 20:01:08 -0500

    added bonus challenge

--- Fundamentals/Fundamentals/hello_world_2.py
+++ Fundamentals/Fundamentals/hello_world_2.py
@@ -14,3 +14,8 @@ fave_food2 = "pizza"
 print("I love to eat {} and {}.".format(fave_food1, fave_food2)) # with .format()
 print(f"I love to eat {fave_food1} and {fave_food2}.") # with an f string
 
+hw = "Hello World!"
+print(str.upper(hw))
+print(str.lower(hw))
+print(str.isalnum(hw))
+print(str.isalpha(hw))
