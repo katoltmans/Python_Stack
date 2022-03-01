a61f34234c6f4e057341cf43197c22b6ec770b39
katoltmans <katoltmans@msn.com>
2022-02-28 19:21:35 -0500

    Initial commit

--- Flask/Fundamentals/understanding_routing/server.py
+++ Flask/Fundamentals/understanding_routing/server.py
@@ -5,7 +5,7 @@ app=Flask(__name__)  #create new instance called app
 def helloWorld():
     return 'Hello World!'
 
-@app.route('')
+
 
 if __name__=="__main__":
     app.run(debug=True) #Run app in debug mode
