460cdaa6b6950d48d5daea06d03427a7b93e62df
katoltmans <katoltmans@msn.com>
2022-03-01 18:27:40 -0500

    Add color change function

--- Flask/Fundamentals/playground/server.py
+++ Flask/Fundamentals/playground/server.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template  # Import flask and render template
-app = Flask(__name__)    
+app = Flask(__name__)
 
-@app.route('/play/<int:num>')          
-def play(num):
-    return render_template('index.html', num=num)  
+#route for initial load screen
+@app.route('/play')
+def play():
+    return render_template('index.html')
+
+
+@app.route('/play/<int:num>')
+def playNum(num):
+    return render_template('index.html', num=num)
+
+@app.route('/play/<int:num>/<string:color>')
+def playChangeColor(num, color):
+    return render_template('index.html', num=num, color=color)
 
 if __name__=="__main__":   # Run app directly
     app.run(debug=True)    # Run app in debug
