f89263a8dca31aad0b2a3603f44685728d25f317
katoltmans <katoltmans@msn.com>
2022-02-28 19:37:33 -0500

    Add dojo and say routes

--- Flask/Fundamentals/understanding_routing/server.py
+++ Flask/Fundamentals/understanding_routing/server.py
@@ -5,7 +5,13 @@ app=Flask(__name__)  #create new instance called app
 def helloWorld():
     return 'Hello World!'
 
+@app.route('/dojo') # Add function - page will say "Dojo!"
+def dojo():
+    return 'Dojo!'
 
+@app.route('/say/<name>/') #Add function to say name typed in URL
+def say(name):
+    return 'Hi ' + name + '!'
 
 if __name__=="__main__":
     app.run(debug=True) #Run app in debug mode
