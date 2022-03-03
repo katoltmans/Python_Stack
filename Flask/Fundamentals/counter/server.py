3abcaec155a29112d4c7f8d9015dbae4a0dccba7
katoltmans <katoltmans@msn.com>
2022-03-03 18:37:01 -0500

    Initial commit

---
+++ Flask/Fundamentals/counter/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = "Shhhhhh..." #secret key set for security purposes
+
+@app.route('/')
+def start():
+    if 'sum' not in session:
+        session['count'] = 0
+    return render_template("index.html")
+
+@app.route('/count', methods=['POST'])
+def count():
+    print("Start count")
+    session['count'] += 1 #Session to count clicks
+    return redirect('/')
+
+@app.route('/destroy_session')
+def destroy_session():
+    session.clear()  #clears the session and resets counter
+    return redirect('/')  #Redirects user back to home route
+
+if __name__=="__main__":
+    app.run(debug=True)
