c790c2cd3718b19f0e06b09008e8097c876a146d
katoltmans <katoltmans@msn.com>
2022-03-05 00:03:48 -0500

    Add display data and reditect functionality

--- Flask/Fundamentals/dojo_survey/server.py
+++ Flask/Fundamentals/dojo_survey/server.py
@@ -4,17 +4,25 @@ app = Flask(__name__)
 app.secret_key = "youwereneverhere"
 
 #Route to display homepage
-@app.route('/')
+@app.route("/")
 def blankSurvey():
     return render_template("index.html")
 
 
 #Route to process input info
-
-
+@app.route("/process", methods=["POST"])
+def processUser():
+    #print(request.form)
+    session['name'] = request.form["name"]
+    session['location'] = request.form["location"]
+    session['language'] = request.form["language"]
+    session['comments'] = request.form["comments"]
+    return redirect("/result")
 
 #Route to display input results
-
+@app.route("/result")
+def displayResults():
+    return render_template("results.html")
 
 if __name__=="__main__":
     app.run(debug=True)
