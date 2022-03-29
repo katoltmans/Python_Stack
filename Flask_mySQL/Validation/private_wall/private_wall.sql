ada1433e7fa7af583c75fef4156d664e815a6ebc
katoltmans <katoltmans@msn.com>
2022-03-29 16:14:48 -0400

    Update sql

--- Flask_mySQL/Validation/private_wall/private_wall.sql
+++ Flask_mySQL/Validation/private_wall/private_wall.sql
@@ -5,4 +5,6 @@ SELECT * FROM users LEFT JOIN friendships ON users.id = friendships.user_id;
 
 SELECT * FROM friendships LEFT JOIN users ON users.id = friendships.user2_id WHERE user_id = 1;
 
-SELECT * FROM messages LEFT JOIN friendships on friendships.user2_id = messages.users_id LEFT JOIN users ON friendships.user_id = users.id WHERE friendships.user_id = 2;
+SELECT * FROM messages LEFT JOIN friendships on friendships.user2_id = messages.users_id LEFT JOIN users ON friendships.user_id = users.id WHERE friendships.user_id = 1;
+
+SELECT * FROM messages LEFT JOIN friendships ON friendships.user2_id = messages.users_id LEFT JOIN users ON users.id = friendships.user2_id WHERE user_id = 1;
